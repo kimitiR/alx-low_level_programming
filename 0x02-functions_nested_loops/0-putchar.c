e9a1d299c94f8009d7c24cfacd1a952f7b3cfc31
root <raphaelkimiti@gmail.com>
2022-12-30 10:55:03 -0800

    adding 0-putchar.c file

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ *
+ * prints out_putchar followed by a newline.
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char ch[8] = "_putchar";
+	int m;
+
+	for (m = 0; m < 8; m++)
+	{
+		_putchar(ch[m]);
+	}
+	_putchar('\n');
+
+	return (0);
+}
