96c627644103af40b9cab2dba646a16c425fe3be
root <raphaelkimiti@gmail.com>
2023-01-01 10:53:17 -0800

    adding 1-main.c file

---
+++ 0x03-debugging/1-main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - causes an infinite loop
+ * Return: Always 0 (Successful)
+ */
+int main(void)
+
+{
+	int i;
+
+	printf("Infinite loop incoming :(\n");
+	printf("Infinite loop avoided! \\o/\n");
+
+	return (0);
+}
