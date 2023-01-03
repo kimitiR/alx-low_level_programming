47c59b10c86eae6594d215b8de599e503f9e6069
root <raphaelkimiti@gmail.com>
2023-01-03 08:30:06 -0800

    updating 11-print_to_98.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -2,49 +2,36 @@
 #include "main.h"
 
 /**
- * print_to_98 - print all natural numbers
- * up to 98
- * @n: number that starts the counting
- * Description: Print all natural numbers up
- * to 98.
- * If the staring number is greater than 98, the
- * function counts down to 98.
- * Otherwise, the function counts up to 98.
+ * print_to_98 - Check Main
+ * @n: A input integer
+ * Description: function that prints all natural numbers
+ * from n to 98, followed by a new line.
+ * Return: Nothing
  */
 void print_to_98(int n)
 {
-  if (n > 98)
-    {
-      while (n >= 98)
+	if (n <= 98)
 	{
-	  if (n == 98)
-	    {
-	      printf("%d\n", n);
-	    }
-	  else
-	    {
-	      printf("%d, ", n);
-	    }
-	  n--;
+		for (; n <= 98; n++)
+		{
+			printf("%d", n);
+
+			if (n == 98)
+				continue;
+			printf(", ");
+		}
+		printf("\n");
 	}
-    }
-  else if (n < 98)
-    {
-      while (n <= 98)
+	else
 	{
-	  if (n == 98)
-	    {
-	      printf("%d\n", n);
-	    }
-	  else
-	    {
-	      printf("%d, ", n);
-	    }
-	  n++;
+		for (; n >= 98; n--)
+		{
+			printf("%d", n);
+
+			if (n == 98)
+				continue;
+			printf(", ");
+		}
+		printf("\n");
 	}
-    }
-  else if (n == 98)
-    {
-      printf("%d\n", n);
-    }
 }
