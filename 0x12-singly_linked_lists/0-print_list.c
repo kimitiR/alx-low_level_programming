bc26544f5ca4be67f0c4b8b6748f32a61026e9b6
kimitiR <raphaelkimiti@gmail.com>
2023-04-27 15:09:18 -0700

    linked list initial commit

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+
+
+
+/**
+ * print_list - function return number of nodes
+ * @h: passed list_t type
+ * Return: number of nodes
+ */
+size_t print_list(const list_t *h)
+{
+	size_t i = 0;
+
+	for (; h != NULL; h = h->next, i++)
+	{
+		if (h->str == NULL)
+		{
+			printf("[0] (nil)\n");
+		}
+		else
+		{
+			printf("[%d] %s\n", h->len, h->str);
+		}
+	}
+	return (i);
+}
