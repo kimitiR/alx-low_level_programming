6deed8eb8e949334ebfdfd73f57932cb11f809c0
kimitiR <raphaelkimiti@gmail.com>
2023-05-02 13:27:06 -0700

    Initial More Singly Linked List Commit

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+
+/**
+ * listint_len - that returns the number of
+ * elements in a linked listint_t list
+ *
+ * @h: points to next node in list
+ * Return: the number of nodes
+ */
+size_t listint_len(const listint_t *h)
+{
+	size_t i = 0;
+
+	for (; h != NULL; h = h->next, i++)
+		;
+	return (i);
+}
