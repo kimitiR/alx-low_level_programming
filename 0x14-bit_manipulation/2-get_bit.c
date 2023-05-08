ef8ab595469bde026ef07c63f761cd5dd6c4fa9c
kimitiR <raphaelkimiti@gmail.com>
2023-05-08 12:31:56 -0700

    fixing 2-get_bit error

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -1,18 +1,20 @@
 #include "main.h"
 
 /**
- * get_bit - gets bit at a specific index
- * @n: number
- * @index: position of bit
+ * get_bit - returns the value of a bit at a given index
+ * @num: number which contains the bit
+ * @index: index at which bit must be found
  *
- * Return: value of bit at index or -1
+ * Return: bit (Success) or -1 (error)
  */
-int get_bit(unsigned long int n, unsigned int index)
+int get_bit(unsigned long int num, unsigned int index)
 {
-	if ((n & (1 << index)) == 0)
-		return (0);
-	else if ((n & (1 << index)) != 0)
-		return (1);
-	else
+	int bit;
+
+	if (index > (sizeof(unsigned long int) * 8 - 1))
 		return (-1);
+
+	bit = (num >> index) & 1;
+
+	return (bit);
 }
